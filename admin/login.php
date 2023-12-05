a7fc2612a8ddfa3a58a5a46dc7135579ce226b2a
nilliyanage <147838734+nilliyanage@users.noreply.github.com>
2023-12-05 23:00:56 +0530

    Update login.php
    space update

--- admin/login.php
+++ admin/login.php
@@ -68,7 +68,6 @@
   </div>  
 </div>
 <script src="plugins/jquery/jquery.min.js"></script>
-
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <script src="dist/js/adminlte.min.js"></script>
