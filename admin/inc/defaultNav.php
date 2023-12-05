309f54a0634eef4fb25b15e90dfe5bb6c99872d1
nilliyanage <147838734+nilliyanage@users.noreply.github.com>
2023-12-05 23:17:45 +0530

    Update defaultNav.php
    space updated

--- admin/inc/defaultNav.php
+++ admin/inc/defaultNav.php
@@ -8,7 +8,6 @@
       <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse order-3" id="navbarCollapse">        
         <ul class="navbar-nav">
           <li class="nav-item">
