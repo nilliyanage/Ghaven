957baccefbc6699f573f9e6c95a8e7008695c897
nilliyanage <147838734+nilliyanage@users.noreply.github.com>
2023-12-05 22:47:50 +0530

    Update login.php
    update

--- admin/login.php
+++ admin/login.php
@@ -83,7 +83,7 @@
 <script src="plugins/jquery/jquery.min.js"></script>
 
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
+
 <script src="dist/js/adminlte.min.js"></script>
 
 <script>
