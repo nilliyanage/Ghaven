ed3d9140929f07f8f615267b08cf6da86e66f065
nilliyanage <147838734+nilliyanage@users.noreply.github.com>
2023-12-05 23:10:36 +0530

    Update topBarNav.php
    comments

--- admin/inc/topBarNav.php
+++ admin/inc/topBarNav.php
@@ -63,7 +63,7 @@
           <li class="nav-item">
             
           </li>
-         <!--  <li class="nav-item">
+         
             <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
             <i class="fas fa-th-large"></i>
             </a>
