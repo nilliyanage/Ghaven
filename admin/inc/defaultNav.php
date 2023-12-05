94948bcc56336b098abe4990c4e5384fcf484fda
nilliyanage <147838734+nilliyanage@users.noreply.github.com>
2023-12-05 23:18:17 +0530

    Update defaultNav.php
    space updated

--- admin/inc/defaultNav.php
+++ admin/inc/defaultNav.php
@@ -22,8 +22,7 @@
               <li><a href="#" class="dropdown-item">Some action </a></li>
               <li><a href="#" class="dropdown-item">Some other action</a></li>
 
-              <li class="dropdown-divider"></li>
-              
+              <li class="dropdown-divider"></li>              
               <li class="dropdown-submenu dropdown-hover">
                 <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
                 <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
